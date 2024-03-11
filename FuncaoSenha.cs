9a9ff3fa356772df4610ac8fe6b379979630420f
Cristian Serena <devcristian02@gmail.com>
2024-03-11 18:19:40 -0300

    Atividade senha com a classe

---
+++ FuncaoSenha.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace Senha
+{
+    public class FuncaoSenha
+    {
+        
+        public string senhaCadastrada;
+        public string senhaDigitada;
+        public string senhaMin;
+        public string senhaMai;
+        public int tentativas = 0;
+
+        public void Senha()
+        {
+            while (tentativas < 5)
+            {
+                Console.Write("Digite a senha Cadastrada: ");
+                senhaDigitada = Console.ReadLine();
+
+                senhaMin = senhaCadastrada.ToLower();
+                senhaMai = senhaCadastrada.ToUpper();
+
+                if (senhaDigitada == senhaMin)
+                {
+                    Console.WriteLine($"Parabéns! Você acertou a senha digitada {senhaMai}");
+                    break;
+                }
+                else if (senhaDigitada == senhaMai)
+                {
+                    Console.WriteLine($"Parabéns! Você acertou a senha digitada {senhaMin}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Senha incorreta. Tente novamente.");
+                    tentativas++;
+                }
+            }
+
+            if (tentativas >= 5)
+            {
+                Console.WriteLine("Limite de tentativas excedido. Tente novamente mais tarde.");
+            }
+        }
+    }
+}
+        
+    
+
---
+++ Senha.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncaoSenha senha = new FuncaoSenha();
+
+            string aste = new string('*', 42);
+
+            Console.WriteLine(aste);
+            Console.WriteLine("Sistema de Senha - Descubra o Segredo ");
+            Console.WriteLine(aste);
+
+            Console.Write("Digite a Senha que você deseja Cadastrar: ");
+            senha.senhaCadastrada = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine(aste);
+            Console.WriteLine("A Senha Digitada foi ****** ");
+            Console.WriteLine(aste);
+
+            senha.Senha();
+
+        }
+    }
+}
+
+
+
